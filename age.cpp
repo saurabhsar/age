f5b7c3c50ea41605d1ec3333e6220c2659a7adf9
Saurabh Araiyer <sizzsa@gmail.com>
2011-07-27 13:08:29 +0530

    second commit

--- age.cpp
+++ age.cpp
@@ -1,80 +1,120 @@
-/**********************************************************************
+/*
+  **********************************************************************
 
-  This program is a console age calculator
-
-  Copyright (C) 2011   Saurabh Araiyer,  sizzsa(at)gmail(dot)com
-  http://www.decapsulatingabstraction.blogspot.com/
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-************************************************************************/
+  *  This program is a console age calculator
+  *
+  *  Copyright (C) 2011   Saurabh Araiyer,  sizzsa(at)gmail(dot)com
+  *  http://www.decapsulatingabstraction.blogspot.com/
+  *
+  *  This program is free software: you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation, either version 3 of the License, or
+  *  any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+  
+  **********************************************************************
+  */
 
 #include <iostream>
-#include <iomanip>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
 int validate(unsigned short int , unsigned short int , unsigned short int);
 int month(unsigned short int, unsigned short int yyyy=0);
 
 int main()
 {
-	cout<<"Please enter your date of birth DD MM YYYY (deliniter: space): ";
 	unsigned short int dd,mm,yyyy;
+	unsigned short int p_dd,p_mm,p_yyyy;
+
+l1:
+	cout<<"Please enter your date of birth DD MM YYYY (delimiter: space): ";
 	cin>>dd>>mm>>yyyy;
 	if( validate(dd,mm,yyyy) )
 		cout<<dd<<"/"<<mm<<"/"<<yyyy;
-	else return 1;
-	cout<<endl<<"Current date: ";
-	unsigned short int p_dd,p_mm,p_yyyy;
-		cin>>p_dd>>p_mm>>p_yyyy;
-	if( validate(p_dd,p_mm,p_yyyy) )
-		cout<<p_dd<<"/"<<p_mm<<"/"<<p_yyyy;
-	else return 1;
+	else 
+	{
+		cout<<"error input !"<<endl;
+		goto l1;
+		return 1;
+	}
 	
-	/// computing the difference
-	unsigned short int d,m,y;
+l2:
+	cout<<endl<<"Current date: ";
+	cin>>p_dd>>p_mm>>p_yyyy;
+	if( validate(p_dd,p_mm,p_yyyy) && p_yyyy < yyyy )
+		cout<<p_dd<<"/"<<p_mm<<"/"<<p_yyyy;
+	else 
+	{
+		cout<<"error input !"<<endl;
+		goto l2;
+		return 1;
+	}
+	
+	/// computing the difference between dates
+	
+	short int d,m,y;
 	y = p_yyyy - yyyy;
 	m = p_mm - mm;
 	d = p_dd - dd;
-	if ( mm >= p_mm)
+	if ( m <= 0 )  //like computing diff between 7 July and 1 May July > May
 	{
 		y--;
 		m += 12;
 	}
-	cout<<endl<<"you are "<<y<<" years, "<<m<<" months old !";
+	if ( d <= 0 ) //similar to the above one, month and day correction
+	{
+		m--;	
+		short int case_month_DOB, case_month_present;
+		case_month_DOB = month ( mm , yyyy );
+		case_month_present = month ( p_mm , p_yyyy );
+	
+		if (!case_month_DOB)
+			d += 31;
+		else switch (case_month_DOB)
+		{
+			case 1:
+				d += 30;
+				break;
+			case 2:
+				d += 28;
+				break;
+			case 3:
+				d += 29;
+				break;
+		}
+	}
+	
+	/// age computation over
+	
+	cout<<endl<<"you are "<<y<<" years, "<<m<<" months "<<d<<" days old !";
 	return 0;
 }
 
 int validate( unsigned short int dd , unsigned short int mm , unsigned short int yyyy)
 {
-	if ( dd > 31 )
+	if ( dd > 31 || dd == 0 || mm == 0  || mm > 12 )
 		return 0;
-	else if ( month(mm) ==1 )
+	switch (month (mm , yyyy))
 	{
-		if ( dd > 30 )
+		case 1:
+			if ( dd > 30 )
+				return 0;
+		case 2:
+			if ( dd > 28 )
 			return 0;
-	}
-	else if ( month(mm) == 2 )
-	{	
-		if ( dd > 28 )
-			return 0;
-	}
-	else if ( month(mm) == 3 )
-	{
-		if ( dd > 29 )
-			return 0;
+		case 3:
+			if ( dd > 29 )
+				return 0;
 	}
 	return 1;
 }
